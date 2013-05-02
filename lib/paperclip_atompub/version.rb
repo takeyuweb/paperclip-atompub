70a28ac376d98ffeb9ecca67b67acf6bcb421890
Yuichi Takeuchi <uzuki05@takeyu-web.com>
2013-05-02 14:23:25 +0900

    自動ロード対策

--- lib/paperclip_atompub/version.rb
+++ lib/paperclip_atompub/version.rb
@@ -1,3 +1,3 @@
 module PaperclipAtompub
-  VERSION = "0.0.3.2"
+  VERSION = "0.0.3.3"
 end
--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -9,7 +9,7 @@ require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 Bundler.require
-require "paperclip_atompub"
+require "paperclip-atompub"
 
 module Dummy
   class Application < Rails::Application
